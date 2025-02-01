ce9c6515874ea6b24d8f4f0b4cd045f4e003953b
alexk101 <akiefer3@utk.edu>
2025-02-01 14:09:06 -0500

    Modified build scripts

--- cmake/FindAdaptiveCpp.cmake
+++ cmake/FindAdaptiveCpp.cmake
@@ -6,34 +6,36 @@
 # ACPP_LIBRARIES - AdaptiveCpp libraries
 # ACPP_VERSION - AdaptiveCpp version
 
-# First check the submodule location
-set(ACPP_SUBMODULE_PATH "${CMAKE_SOURCE_DIR}/../third_party/AdaptiveCpp")
+# First build and install AdaptiveCpp from submodule if not already done
+set(ACPP_SUBMODULE_PATH "${CMAKE_SOURCE_DIR}/third_party/AdaptiveCpp")
+if(EXISTS "${ACPP_SUBMODULE_PATH}" AND NOT EXISTS "${ACPP_SUBMODULE_PATH}/build")
+    message(STATUS "Building AdaptiveCpp from submodule...")
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -B build -DWITH_ROCM_BACKEND=ON -DROCM_PATH=/opt/rocm
+        WORKING_DIRECTORY ${ACPP_SUBMODULE_PATH}
+    )
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} --build build --target install
+        WORKING_DIRECTORY ${ACPP_SUBMODULE_PATH}
+    )
+endif()
 
+# Now look for the installed AdaptiveCpp
 find_path(ACPP_INCLUDE_DIR
     NAMES sycl/sycl.hpp
     PATHS
-        ${ACPP_SUBMODULE_PATH}/include
+        ${ACPP_SUBMODULE_PATH}/build/install/include/AdaptiveCpp
         ${ACPP_HOME}/include
-        /opt/adaptivecpp/include
-        /usr/local/adaptivecpp/include
     NO_DEFAULT_PATH
 )
 
 find_library(ACPP_LIBRARY
     NAMES sycl
     PATHS
-        ${ACPP_SUBMODULE_PATH}/lib
+        ${ACPP_SUBMODULE_PATH}/build/install/lib
         ${ACPP_HOME}/lib
-        /opt/adaptivecpp/lib
-        /usr/local/adaptivecpp/lib
     NO_DEFAULT_PATH
 )
-
-# If not found in submodule or specified paths, try system paths
-if(NOT ACPP_INCLUDE_DIR OR NOT ACPP_LIBRARY)
-    find_path(ACPP_INCLUDE_DIR NAMES sycl/sycl.hpp)
-    find_library(ACPP_LIBRARY NAMES sycl)
-endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(AdaptiveCpp
@@ -45,4 +47,5 @@ find_package_handle_standard_args(AdaptiveCpp
 if(AdaptiveCpp_FOUND)
     set(ACPP_INCLUDE_DIRS ${ACPP_INCLUDE_DIR})
     set(ACPP_LIBRARIES ${ACPP_LIBRARY})
+    set(AdaptiveCpp_DIR ${ACPP_SUBMODULE_PATH}/build/install/lib/cmake/AdaptiveCpp)
 endif() 
